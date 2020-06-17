c6f9aed0eb132df2daddfee99d81a27d5c17b5ca
Kymane Llewellyn <enamyk@gmail.com>
2020-06-17 07:54:56 -0400

    Markers

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -1,5 +1,6 @@
 var map;
 function initMap() {
+    var marker;
     var directionsService = new google.maps.DirectionsService();
     var directionsRenderer = new google.maps.DirectionsRenderer();
 
@@ -10,11 +11,23 @@ function initMap() {
 
     directionsRenderer.setMap(map);
 
+    function placeMarker(location) {
+      if (marker) {
+        marker.setPosition(location);
+      } else {
+        marker = new google.maps.Marker({
+          position:location,
+          map:map,
+        });
+      }
+    }
 
     //Add a listener. This function runs when the 'click' event occurs on the map object.
     map.addListener("click", function (event) {
         var latitude = event.latLng.lat();
         var longitude = event.latLng.lng();
         console.log(latitude + ', ' + longitude);
+        //place marker
+        placeMarker(event.latLng);
     });
 }
