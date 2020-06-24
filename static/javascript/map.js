9f9ad334310de923ab8e571a9d24c3655866cc81
Kymane Llewellyn <enamyk@gmail.com>
2020-06-24 12:03:54 -0400

    origin and destination

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -1,9 +1,9 @@
-var map;
-var origin, destination;
+var map, origin, destination, currPos, latitude, longitude, directionsService, directionsRenderer;
+
+
 function initMap() {
-    var marker;
-    var directionsService = new google.maps.DirectionsService();
-    var directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer();
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 38.034004119, lng: -78.50953967324405},
@@ -12,21 +12,17 @@ function initMap() {
     }
   );
 
-    setupAutoComplete(map);
+  //  setupAutoComplete(map);
 
     directionsRenderer.setMap(map);
 
 
-
-
-
-
-    function placeMarker(location) {
-        if (marker) {
-            marker.setPosition(location);
+    function setOrigin(marker) {
+        if (currPos) {
+            currPos.setPosition(marker);
         } else {
-            marker = new google.maps.Marker({
-                position:location,
+            currPos = new google.maps.Marker({
+                position:marker,
                 map:map,
             });
         }
@@ -35,11 +31,11 @@ function initMap() {
 
     //Add a listener. This function runs when the 'click' event occurs on the map object.
     map.addListener("click", function (event) {
-        var latitude = event.latLng.lat();
-        var longitude = event.latLng.lng();
-        console.log(latitude + ', ' + longitude);
+        latitude = event.latLng.lat();
+        longitude = event.latLng.lng();
+        //currPos = new google.maps.LatLng(latitude,longitude);
         //place marker
-        placeMarker(event.latLng);
+        setOrigin(event.latLng);
     });
 
     // Autocomplete
@@ -50,11 +46,12 @@ function initMap() {
     **/
 
     //currPos is the current Location of the user
-    var currPos;
+    //var currPos;
     //currPosFail ouputs in the HTML if there was a problem getting the user's current location.
-    var currPosFail = document.getElementById("currPositionGrab");
+    //var currPosFail = document.getElementById("currPositionGrab");
 
     //Upon loading, request user location access, printing if an error occurred below the map
+/*
     window.onload = function(){
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(getCurrPos, currPosErr);
@@ -64,37 +61,109 @@ function initMap() {
         }
     }
 
-    function getCurrPos(pos){
-        var latitude = pos.coords.latitude;
-        var longitude = pos.coords.longitude;
-
-        /**
-        unsure about this part (below)
-        **/
-
-        //create google LatLng object
-        currPos = new google.maps.LatLng(latitude,longitude);
-
-        //Put on map as marker (for now)
-        var currPosMarker = new google.maps.Marker({
-            position: currPos,
-            map: map
-        });
-
-        /**end uncertainty**/
-    }
-
+*/
+/*
     function currPosErr(){
         currPosFail.innerHTML = "There was a problem getting your location";
     }
     var autocomplete = new google.maps.places.autocomplete(input, options);
+*/
 }
 
 
 
 //Find Coordinates of Starting location
-
+/*
 function setOrigin() {
   origin = document.getElementById("pac-input").value;
   console.log(origin);
 }
+*/
+
+
+
+    function genRoute(distance) {
+      let origin, destination;
+      let lat_origin = latitude;
+      let long_origin = longitude;
+      let distanceTo = parseFloat(distance/2);
+
+      console.log(lat_origin);
+      directions = [
+         { direction : "north", latitude: '', longitude: ''}, { direction : "south", latitude: '', longitude: ''},
+         { direction : "east", latitude: '', longitude: ''}, { direction : "west", latitude: '', longitude: ''},
+         { direction : "northeast", latitude: '', longitude: ''}, { direction : "northwest", latitude: '', longitude: ''},
+         { direction : "southeast", latitude: '', longitude: ''}, { direction : "southwest", latitude: '', longitude: ''},
+      ];
+
+      let randomDirection = directions[Math.floor(Math.random() * directions.length)];
+
+
+      if (randomDirection.direction === 'north') {
+        randomDirection.latitude = lat_origin;
+        randomDirection.longitude = long_origin + distanceTo;
+      }
+      else if (randomDirection.direction === 'south'){
+        randomDirection.latitude = lat_origin;
+        randomDirection.longitude = long_origin - distanceTo;
+      }
+      else if (randomDirection.direction === 'east'){
+        randomDirection.latitude = lat_origin + distanceTo;
+        randomDirection.longitude = long_origin;
+      }
+      else if (randomDirection.direction === 'west'){
+        randomDirection.latitude = lat_origin - distanceTo;
+        randomDirection.longitude = long_origin;
+      }
+      else if (randomDirection.direction === 'northeast'){
+        randomDirection.latitude = lat_origin + distanceTo;
+        randomDirection.longitude = long_origin + distanceTo;
+      }
+      else if (randomDirection.direction === 'northwest'){
+        randomDirection.latitude = lat_origin - distanceTo;
+        randomDirection.longitude = long_origin + distanceTo;
+      }
+      else if (randomDirection.direction === 'southeast'){
+        randomDirection.latitude = lat_origin + distanceTo;
+        randomDirection.longitude = long_origin - distanceTo;
+      }
+      else if (randomDirection.direction === 'southwest'){
+        randomDirection.latitude = lat_origin - distanceTo;
+        randomDirection.longitude = long_origin - distanceTo;
+      }
+
+      //destination = {latitude: randomDirection.latitude, longitude: randomDirection.longitude};
+      let lat_dest = randomDirection.latitude;
+      let long_dest = randomDirection.longitude;
+
+      //console.log(origin)
+      //console.log(destination)
+      //let directionsService = new google.maps.DirectionsService();
+      //let directionsRenderer = new google.maps.DirectionsRenderer();
+
+      origin = "" + lat_origin + "" + long_origin;
+      destination = "" + lat_dest + "" + long_dest;
+
+      let request = {
+        origin: origin,
+        destination: destination,
+        travelMode: 'WALKING'
+      };
+
+      directionsService.route(request, function(result, status){
+          if(status === "OK"){
+            directionsRenderer.setDirections(result);
+          }
+      });
+    }
+
+
+
+
+
+    function isNumberKey(evt){
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57))
+            return false;
+        return true;
+    }
