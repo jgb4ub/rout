71b3bef1fa8e7802ff79f4f11fc4ee3c846f630b
cfoutz <crf2fj@virginia.edu>
2020-06-29 17:35:19 -0400

    Small edits

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -301,8 +301,6 @@ function genRoute(distance) {
 
 }
 
-
-
 function isNumberKey(evt){
     var charCode = (evt.which) ? evt.which : evt.keyCode;
     if (charCode > 31 && (charCode < 48 || charCode > 57))
