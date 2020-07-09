4592db6c71d3f382127d94cc8f7684a992302827
ChloeEstrada <60328305+chloeestrada@users.noreply.github.com>
2020-07-09 17:31:01 -0400

    Map feature

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -528,7 +528,93 @@ function iterativeRouting(requestData, result, counter){
 
 function callOutput(directResult){
     directionsRenderer.setDirections(directResult);
+    elevationCreator(directResult);
 }
+
+function elevationCreator(directResult){
+    // Load the Visualization API and the columnchart package.
+    google.load('visualization', '1', {packages: ['columnchart']});
+
+    var path = pathData(directResult);
+
+    // var path = [
+    //     {lat: 36.579, lng: -118.292},  // Mt. Whitney
+    //     {lat: 36.606, lng: -118.0638},  // Lone Pine
+    //     {lat: 36.433, lng: -117.951},  // Owens Lake
+    //     {lat: 36.588, lng: -116.943},  // Beatty Junction
+    //     {lat: 36.34, lng: -117.468},  // Panama Mint Springs
+    //     {lat: 36.24, lng: -116.832}];  // Badwater, Death Valley
+
+
+    // Create an ElevationService.
+    var elevator = new google.maps.ElevationService;
+
+    // Draw the path, using the Visualization API and the Elevation service.
+    displayPathElevation(path, elevator, map);
+}
+
+
+function pathData(directResult){
+    var route = directResult.routes[0]; //route is a directionRoute object
+    return route.overview_path;
+    // for (x in route.legs[]) {
+    //     path.push();
+    // }
+}
+
+
+function displayPathElevation(path, elevator, map) {
+  // Display a polyline of the elevation path.
+    new google.maps.Polyline({
+        path: path,
+        strokeColor: '#0000CC',
+        strokeOpacity: 0.4,
+        map: map
+    });
+
+    // Create a PathElevationRequest object using this array.
+    // Ask for 256 samples along that path.
+    // Initiate the path request.
+    elevator.getElevationAlongPath({
+        'path': path,
+        'samples': 256
+    }, plotElevation);
+}
+
+// Takes an array of ElevationResult objects, draws the path on the map
+// and plots the elevation profile on a Visualization API ColumnChart.
+function plotElevation(elevations, status) {
+    var chartDiv = document.getElementById('elevation_chart');
+    if (status !== 'OK') {
+    // Show the error code inside the chartDiv.
+        chartDiv.innerHTML = 'Cannot show elevation: request failed because ' + status;
+        return;
+    }
+    // Create a new chart in the elevation_chart DIV.
+    var chart = new google.visualization.ColumnChart(chartDiv);
+
+    // Extract the data from which to populate the chart.
+    // Because the samples are equidistant, the 'Sample'
+    // column here does double duty as distance along the
+    // X axis.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Sample');
+    data.addColumn('number', 'Elevation');
+    for (var i = 0; i < elevations.length; i++) {
+        data.addRow(['', elevations[i].elevation]);
+    }
+
+    // Draw the chart using the data within its DIV.
+    chart.draw(data, {
+        height: 150,
+        legend: 'none',
+        titleY: 'Elevation (m)'
+    });
+}
+
+
+
+
 
 function tooShort(dirResult){
     if (pathDifferenceCalc(dirResult)<(-.05*dist)){
